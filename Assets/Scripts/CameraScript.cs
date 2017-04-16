0dd85d0f708963fa6973e07f48cbc04ed8aa5a68
kgc00Zenbook <kirbygc00@gmail.com>
2017-04-16 00:15:28 -0400

    Added Reference Scripts

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    public Transform pos3;
+    public Transform pos4;
+    public int camNum = 1;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    { 
+    }
+
+    public void ChangeCamera()
+    {
+        Debug.Log("ChangeCamera: " + camNum);
+        if (camNum == 1)
+        {
+            transform.position = pos1.position;
+            transform.rotation = (pos1.rotation);
+        }
+        else if (camNum == 2)
+        {
+            transform.position = pos2.position;
+            transform.rotation = (pos2.rotation);
+        }
+        else if (camNum == 3)
+        {
+            transform.position = pos3.position;
+            transform.rotation = (pos3.rotation);
+        }
+        else if (camNum == 4)
+        {
+            Debug.Log("Worked");
+            transform.position = pos4.position;
+            transform.rotation = (pos4.rotation);
+        }
+    } 
+}
